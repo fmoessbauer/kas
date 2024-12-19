fmoessbauer/kas

commit bbbc087ca1ab81c3d5d8ac9f91557d8891316e26

fix(run_cmd): handle not-executable errors
In case the we call the run_cmd wrapper with fail=False, it must not
throw an exception if the command that should be executed does not exist
or cannot be executed.

Fixes: c125f1efc "fix: do not run sync commands via event loop"
Reported-by: Konrad Weihmann <konrad.weihmann@avnet.eu>
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/libkas.py
@@ -185,9 +185,18 @@ def run_cmd(cmd, cwd, env=None, fail=True):
     cmdstr = ' '.join(cmd)
     logging.debug('%s$ %s', cwd, cmdstr)
 
-    ret = subprocess_run(cmd, env=env, cwd=cwd, stdout=PIPE)
-    if ret.returncode and fail:
-        raise CommandExecError(cmd, ret.returncode)
+    try:
+        ret = subprocess_run(cmd, env=env, cwd=cwd, stdout=PIPE)
+        if ret.returncode and fail:
+            raise CommandExecError(cmd, ret.returncode)
+    except FileNotFoundError as ex:
+        if fail:
+            raise ex
+        return (errno.ENOENT, str(ex))
+    except PermissionError as ex:
+        if fail:
+            raise ex
+        return (errno.EPERM, str(ex))
     return (ret.returncode, ret.stdout.decode('utf-8'))
 
 

commit 8e494fed3d0993c15fa4e410302822d06deba70d

run_cmd: do not emit stderr to console
During repo-root probing errors are expected. However, these are emitted
to the caller terminal as we do not internally redirect them. We now
also caputure stderr on run_cmd.

Sample output:
  2024-12-11 09:59:34 - INFO     - kas 4.6 started
  fatal: not a git repository (or any parent up to mount point /)
  Stopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set).
  abort: no repository found in '/work' (.hg not found)
  fatal: not a git repository (or any parent up to mount point /)
  Stopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set).
  abort: no repository found in '/work' (.hg not found)
  2024-12-11 09:59:34 - INFO     - Using /repo as root for repository cip-core

Fixes: bbbc087c ("fix(run_cmd): handle not-executable errors")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

kas/libkas.py
@@ -186,7 +186,7 @@ def run_cmd(cmd, cwd, env=None, fail=True):
     logging.debug('%s$ %s', cwd, cmdstr)
 
     try:
-        ret = subprocess_run(cmd, env=env, cwd=cwd, stdout=PIPE)
+        ret = subprocess_run(cmd, env=env, cwd=cwd, stdout=PIPE, stderr=PIPE)
         if ret.returncode and fail:
             raise CommandExecError(cmd, ret.returncode)
     except FileNotFoundError as ex: