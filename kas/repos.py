fmoessbauer/kas

commit 3e5b0c20ab32ef0a52f61c02ec704280c9f53ae6

add support to create provenance build attestations
This patch adds support to create SLSA provenance attestations for
the kas build. The attestation captures data about the build
environment, the kas cmdline, all direct dependencies, as well as the
specified artifacts. The attestation predicate is created as
kas-build.provenance.json in the build/attestation directory.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/repos.py
@@ -510,6 +510,10 @@ class GitRepo(RepoImpl):
         Provides the git functionality for a Repo.
     """
 
+    @staticmethod
+    def get_type():
+        return 'git'
+
     def remove_ref_prefix(self, ref):
         ref_prefix = 'refs/'
         return ref[ref.startswith(ref_prefix) and len(ref_prefix):]
@@ -595,6 +599,10 @@ class MercurialRepo(RepoImpl):
     """
         Provides the hg functionality for a Repo.
     """
+
+    @staticmethod
+    def get_type():
+        return 'mercurial'
 
     def add_cmd(self):
         return ['hg', 'add']

docs/command-line/environment-variables.inc
@@ -128,11 +128,12 @@ Variables Glossary
 +--------------------------+--------------------------------------------------+
 | ``CI_SERVER_HOST``       | Environment variables from gitlab CI, if set     |
 | ``CI_JOB_TOKEN``         | .netrc is configured to allow fetching from      |
-| (K)                      | the gitlab instance. An entry will be appended   |
-|                          | in case ``NETRC_FILE`` was given as well. Note   |
+| ``CI_JOB_URL``           | the gitlab instance. An entry will be appended   |
+| (K)                      | in case ``NETRC_FILE`` was given as well. Note   |
 |                          | that if the file already contains an entry for   |
 |                          | that host most tools would probably take that    |
-|                          | first one.                                       |
+|                          | first one. The job url is added to the           |
+|                          | provenance attestation (if enabled).             |
 +--------------------------+--------------------------------------------------+
 | ``GITHUB_ACTIONS``       | Environment variables from github actions. If    |
 | (K)                      | set to `true`, `.gitconfig` is automatically     |

docs/conf.py
@@ -343,6 +343,9 @@ man_pages = [
      [author], 1),
     ('_man/kas-credentials', 'kas-credentials', 'kas credential handling',
      [author], 1),
+    ('_man/kas-build-attestation',
+     'kas-build-attestation', 'working with SLSA / in-toto attestations',
+     [author], 1),
 ]
 
 # If true, show URL addresses after external links.

kas/attestation.py
@@ -1,0 +1,229 @@
+# kas - setup tool for bitbake based projects
+#
+# Copyright (c) Siemens AG, 2024
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+    This module provides infrastructure to generate provenance attestation
+    of the build process.
+"""
+
+import os
+import distro
+import logging
+import hashlib
+import base64
+import sys
+from enum import Enum
+from urllib.parse import urlparse
+from pathlib import Path
+from datetime import timezone
+from kas import __version__ as KASVERSION
+
+
+SLSA_PROVENANCE_TYPE = 'https://slsa.dev/provenance/v1'
+KAS_BUILDER_ID = 'https://github.com/siemens/kas'
+KAS_BUILD_TYPE = 'https://kas.readthedocs.io/en' \
+                 f'/{KASVERSION}/userguide.html#project-configuration'
+INTOTO_STATEMENT_TYPE = 'https://in-toto.io/Statement/v1'
+
+
+def date_to_rfc3339(dt):
+    return dt.astimezone(timezone.utc) \
+             .strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
+
+def file_digest_slow(f, algorithm):
+    """
+        Implementation of hashlib.file_digest for python < 3.11
+    """
+    hash = hashlib.new(algorithm)
+    while True:
+        data = f.read()
+        if not data:
+            break
+        hash.update(data)
+    return hash
+
+
+class Provenance:
+    """
+        Create a build attestation predicate in SLSA provenance format
+        for a kas build.
+    """
+    class Mode(Enum):
+        MIN = 1
+        MAX = 10
+
+    def __init__(self, ctx, t_started, t_finished, mode=Mode.MIN):
+        self._ctx = ctx
+        self._t_started = t_started
+        self._t_finished = t_finished
+        self._mode = mode
+
+    @staticmethod
+    def _url_with_protocol(url):
+        if url.startswith('git@'):
+            return f'ssh://{url}'
+        if url.startswith('ssh://'):
+            return f'{url}'
+        if url.startswith('http://') or url.startswith('https://'):
+            return f'{url}'
+
+    @staticmethod
+    def _strip_credentials(url):
+        """
+            Returns the url with all credentials removed (best effort)
+        """
+        if not url.startswith('http://') and not url.startswith('https://'):
+            return url
+        parsed = urlparse(url)
+        if parsed.username and parsed.password:
+            url = url.replace(f'{parsed.username}:{parsed.password}@', '')
+        return url
+
+    @staticmethod
+    def _get_filetype(f: Path):
+        if f.suffix == '.json':
+            return 'json'
+        return 'yaml'
+
+    def _make_relative_path(self, path: Path):
+        top_repo_path = Path(self._ctx.config.handler.get_top_repo_path())
+        workdir = Path(self._ctx.kas_work_dir)
+
+        # is_relative_to implementation for python < 3.9
+        try:
+            return path.relative_to(workdir)
+        except ValueError:
+            return path.relative_to(top_repo_path)
+
+    def type_(self):
+        return SLSA_PROVENANCE_TYPE
+
+    def as_dict(self):
+        res_deps = []
+        for r in self._ctx.config.get_repos():
+            if r.operations_disabled:
+                if not r.url or not r.revision:
+                    continue
+            digest = {'sha1': r.revision}
+            annotations = {
+                'dirty': r.dirty,
+                'layers': [str(Path(layer).relative_to(r.path))
+                           for layer in r.layers]
+            }
+            cleanurl = self._strip_credentials(r.url)
+            dep = {
+                'name': r.name,
+                'uri': f'{r.get_type()}+{self._url_with_protocol(cleanurl)}',
+                'digest': digest,
+                'annotations': annotations
+            }
+            res_deps.append(dep)
+
+        # (abspath, relpath)
+        config_files = [(Path(c), self._make_relative_path(Path(c)))
+                        for c in self._ctx.config.filenames]
+        for ca, cr in config_files:
+            with open(ca, 'rb') as f:
+                content = f.read()
+            rd = {
+                'name': str(cr),
+                'content': base64.b64encode(content).decode('utf-8'),
+                'mediaType': f'application/vnd.kas+{self._get_filetype(ca)}'
+            }
+            res_deps.append(rd)
+
+        bd = {
+            'buildType': KAS_BUILD_TYPE,
+            'externalParameters': {
+                'command': self._ctx.args.cmd,
+                'config': [str(c) for _, c in config_files],
+                'target': self._ctx.args.target,
+                'task': self._ctx.args.task,
+                'extra_bitbake_args': self._ctx.args.extra_bitbake_args,
+            },
+            'internalParameters': {},
+            'resolvedDependencies': res_deps
+        }
+        if self._mode == self.Mode.MAX:
+            bd['internalParameters']['env'] = \
+                self._ctx.config.get_environment()
+        b_versions = {
+            'kas': KASVERSION,
+            'distro.name': distro.id(),
+            'distro.version': distro.version()
+        }
+
+        rd = {
+            'builder':
+            {
+                'id': KAS_BUILDER_ID,
+                'version': b_versions
+            },
+            'metadata': {
+                'invocationId': os.environ.get('CI_JOB_URL', ''),
+                'startedOn': date_to_rfc3339(self._t_started),
+                'finishedOn': date_to_rfc3339(self._t_finished),
+            },
+            'byproducts': []
+        }
+        p = {
+            'buildDefinition': bd,
+            'runDetails': rd
+        }
+        return p
+
+
+class Statement:
+    """
+        Create a statement in in-toto format for a kas build.
+    """
+    def __init__(self, predicate, ctx, t_started, t_finished):
+        self._predicate = predicate
+        self._ctx = ctx
+        self._t_started = t_started
+        self._t_finished = t_finished
+
+    def as_dict(self):
+        pt = self._predicate.type_()
+        pp = self._predicate.as_dict()
+        subjects = []
+        for n, s in self._ctx.config.get_artifacts():
+            fullpath = Path(self._ctx.build_dir) / s
+            with open(fullpath, "rb") as f:
+                if sys.version_info < (3, 11):
+                    digest = file_digest_slow(f, "sha256")
+                else:
+                    digest = hashlib.file_digest(f, "sha256")
+            rd = {
+                'name': s.name,
+                'digest': {'sha256': digest.hexdigest()}
+            }
+            subjects.append(rd)
+        if len(subjects) == 0:
+            logging.warning('Attestation does not contain any subjects.')
+        st = {
+            '_type': INTOTO_STATEMENT_TYPE,
+            'subject': subjects,
+            'predicateType': pt,
+            'predicate': pp
+        }
+        return st

kas/plugins/build.py
@@ -1,6 +1,6 @@
 # kas - setup tool for bitbake based projects
 #
-# Copyright (c) Siemens AG, 2017-2018
+# Copyright (c) Siemens AG, 2017-2024
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -26,15 +26,16 @@
     build environment and then invoke bitbake to build the targets selected
     in the chosen config file.
 
-    For example, to build the configuration described in the file
-    ``kas-project.yml`` you could run::
-
-        kas build kas-project.yml
+    When running with ``--provenance <true|mode=...>`` kas will generate an
+    provenance attestation for the build. The attestation will be stored in
+    ``attestation/kas-build.provenance.json`` in the build directory.
+    For details about provenance, see the build attestation chapter.
 """
 
 import logging
 import subprocess
 import sys
+import json
 from pathlib import Path
 from datetime import datetime
 from kas.context import create_global_context
@@ -44,9 +45,11 @@ from kas.libkas import setup_parser_keep_config_unchanged_arg
 from kas.libcmds import Macro, Command
 from kas.libkas import setup_parser_common_args
 from kas.kasusererror import CommandExecError
+from kas.attestation import Provenance, Statement
+
 
 __license__ = 'MIT'
-__copyright__ = 'Copyright (c) Siemens AG, 2017-2018'
+__copyright__ = 'Copyright (c) Siemens AG, 2017-2024'
 
 
 class Build:
@@ -77,6 +80,9 @@ class Build:
                             help='Select target to build')
         parser.add_argument('-c', '--cmd', '--task', dest='task',
                             help='Select which task should be executed')
+        parser.add_argument('--provenance',
+                            choices=['true', 'mode=min', 'mode=max'],
+                            help='Enable provenance attestation generation')
 
     def run(self, args):
         """
@@ -106,6 +112,21 @@ class BuildCommand(Command):
 
     def __str__(self):
         return 'build'
+
+    def _generate_attestation(self, ctx,
+                              time_started, time_finished,
+                              mode):
+        """
+            Generate the provenance attestation for the build.
+        """
+        predicate = Provenance(ctx, time_started, time_finished,
+                               mode)
+        stmt = Statement(predicate, ctx, time_started, time_finished).as_dict()
+        att_dir = Path(ctx.build_dir) / 'attestation'
+        att_dir.mkdir(parents=True, exist_ok=True)
+        with open(att_dir / 'kas-build.provenance.json', 'w') as f:
+            f.write(json.dumps(stmt, indent=4))
+            f.write('\n')
 
     def _warn_artifact_timestamp(self, ctx, artifacts,
                                  t_started, t_finished):
@@ -145,6 +166,11 @@ class BuildCommand(Command):
         artifacts = ctx.config.get_artifacts()
         self._warn_artifact_timestamp(ctx, artifacts,
                                       time_started, time_finished)
+
+        if ctx.args.provenance:
+            mode = Provenance.Mode.MAX if ctx.args.provenance == 'mode=max' \
+                else Provenance.Mode.MIN
+            self._generate_attestation(ctx, time_started, time_finished, mode)
 
 
 __KAS_PLUGINS__ = [Build]

kas/plugins/menu.py
@@ -293,6 +293,7 @@ class Menu:
             build_args.task = None
             build_args.extra_bitbake_args = []
             build_args.skip = None
+            build_args.provenance = False
 
             Build().run(build_args)
 

commit 657d4e2c75f41eef88a44b6d61f1571994141baa

align repo type identifier with SPDX package information spec
For mercurial repos the repo type identifier is "hg" (not "mercurial")
according to spdx-spec/v2.3/package-information. As this information is
used in the attestation data, we need to align it.

To document this alignment, we add an abstract method with a
docstring mentioning this relation.

Fixes: 3e5b0c20a ("add support to create provenance build attestations")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

kas/repos.py
@@ -157,6 +157,13 @@ class Repo:
                               cwd=self.path, fail=False)
         return bool(output)
 
+    @staticmethod
+    def get_type():
+        """
+            Repo type as defined in spdx-spec/v2.3/package-information
+        """
+        raise NotImplementedError("Repo type not implemented")
+
     def __str__(self):
         if self.commit and (self.tag or self.branch):
             refspec = f'{self.commit}({self.tag or self.branch})'
@@ -675,7 +682,7 @@ class MercurialRepo(RepoImpl):
 
     @staticmethod
     def get_type():
-        return 'mercurial'
+        return 'hg'
 
     def add_cmd(self):
         return ['hg', 'add']