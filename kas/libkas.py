fmoessbauer/kas

commit e8e5ea54af814f955edceb2104ad961479fcd2e5

run_cmd: add support to capture stderr as well
For some operations we need to capture stderr as well to give meanigful
error messages back to the user. To allow that, we add the
capture_stderr parameter to the run_cmd* functions which returns a
triple instead of a tuple when requested.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/libkas.py
@@ -109,7 +109,15 @@ async def _read_stream(stream, callback):
             break
 
 
-async def run_cmd_async(cmd, cwd, env=None, fail=True, liveupdate=False):
+def _filter_stderr(capture_stderr, ret, out, err):
+    if capture_stderr:
+        return (ret, out, err or '')
+    else:
+        return (ret, out)
+
+
+async def run_cmd_async(cmd, cwd, env=None, fail=True, liveupdate=False,
+                        capture_stderr=False):
     """
         Run a command asynchronously.
     """
@@ -134,11 +142,11 @@ async def run_cmd_async(cmd, cwd, env=None, fail=True, liveupdate=False):
     except FileNotFoundError as ex:
         if fail:
             raise ex
-        return (errno.ENOENT, str(ex))
+        return _filter_stderr(capture_stderr, errno.ENOENT, str(ex))
     except PermissionError as ex:
         if fail:
             raise ex
-        return (errno.EPERM, str(ex))
+        return _filter_stderr(capture_stderr, errno.EPERM, str(ex))
 
     # Process termination is a complicated thing. We need to ensure that
     # the event-loop ThreadedChildWatcher thread fires before the loop is
@@ -170,10 +178,11 @@ async def run_cmd_async(cmd, cwd, env=None, fail=True, liveupdate=False):
         logging.error(msg)
         raise CommandExecError(cmd, ret)
 
-    return (ret, ''.join(logo.stdout))
+    return _filter_stderr(capture_stderr, ret,
+                          ''.join(logo.stdout), ''.join(logo.stderr))
 
 
-def run_cmd(cmd, cwd, env=None, fail=True):
+def run_cmd(cmd, cwd, env=None, fail=True, capture_stderr=False):
     """
         Runs a command synchronously.
     """
@@ -193,7 +202,9 @@ def run_cmd(cmd, cwd, env=None, fail=True):
         if fail:
             raise ex
         return (errno.EPERM, str(ex))
-    return (ret.returncode, ret.stdout.decode('utf-8'))
+    return _filter_stderr(capture_stderr, ret.returncode,
+                          ret.stdout.decode('utf-8'),
+                          ret.stderr.decode('utf-8'))
 
 
 def find_program(paths, name):

commit 9052cef37479853b895b3acbf7289f402591c18b

run_cmd: return triple on error when capturing stderr
In case we run run_cmd with capture_stderr=True and run into an error
like FileNotFound or Permission, we also need to return a triple instead
of a tuple. Otherwise the return type is not the same depending on if an
error occured or not. This further fixes the issue that _filter_stderr
is called without a fourth parameter (stderr) in run_cmd_async.

These fixes do not have any user-facing impact.

Fixes: e8e5ea54a ("run_cmd: add support to capture stderr as well")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

kas/libkas.py
@@ -109,7 +109,7 @@ async def _read_stream(stream, callback):
             break
 
 
-def _filter_stderr(capture_stderr, ret, out, err):
+def _filter_stderr(capture_stderr, ret, out, err=None):
     if capture_stderr:
         return (ret, out, err or '')
     else:
@@ -197,11 +197,11 @@ def run_cmd(cmd, cwd, env=None, fail=True, capture_stderr=False):
     except FileNotFoundError as ex:
         if fail:
             raise ex
-        return (errno.ENOENT, str(ex))
+        return _filter_stderr(capture_stderr, errno.ENOENT, str(ex))
     except PermissionError as ex:
         if fail:
             raise ex
-        return (errno.EPERM, str(ex))
+        return _filter_stderr(capture_stderr, errno.EPERM, str(ex))
     return _filter_stderr(capture_stderr, ret.returncode,
                           ret.stdout.decode('utf-8'),
                           ret.stderr.decode('utf-8'))