fmoessbauer/kas

commit f4beea37fe840439e3eaa3d399e0795f6dcf7de4

graceful cancellation of external commands
Instead of just propagating the SIGINT / SIGTERM to the subprocess
executor, we use the asyncio infrastructure to send a cancellation event
to all tasks. By using asyncio.gather instead of .wait we ensure all
downstream tasks are cancelled on the first failure (or cancellation
event), making sure to not leave any orphaned tasks behind.
To make sure the cancellation signal (e.g. SIGINT) arrives in the
correct order, we spawn the subprocesses in their own process group.
Also, we shield the asyncio process related tasks to not cancel them
directly by asyncio, but instead send a user-defined (SIGINT) signal to
the process, so we can wait for the termination.
This leads to the following signal chain:

1. user presses CTRL^C -> SIGINT sent to Python process
2. signal handler cancels all tasks (except the shielded ones)
3. asyncio raises a CancelledError inside coroutines, which results in
   a SIGINT being sent to the external process
4. execution continues once all external processes terminated

As we now cleanly terminate, we no longer need the atexit handler.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kas/libkas.py
@@ -133,7 +133,8 @@ async def run_cmd_async(cmd, cwd, env=None, fail=True, liveupdate=False):
             cwd=cwd,
             env=env,
             stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE)
+            stderr=asyncio.subprocess.PIPE,
+            preexec_fn=os.setpgrp)
     except FileNotFoundError as ex:
         if fail:
             raise ex
@@ -143,12 +144,26 @@ async def run_cmd_async(cmd, cwd, env=None, fail=True, liveupdate=False):
             raise ex
         return (errno.EPERM, str(ex))
 
+    # Process termination is a complicated thing. We need to ensure that
+    # the event-loop ThreadedChildWatcher thread fires before the loop is
+    # terminated. The best we can do it to ask the process to terminate
+    # (SIGINT) and wait for it. We need to shield the process wait to avoid
+    # that it is killed by the cancellation of the task, as we want a
+    # controlled termination. Forced terminations can leak an orphaned process.
+    # https://github.com/pytest-dev/pytest-asyncio/issues/708#issuecomment-1868488942
     tasks = [
         asyncio.ensure_future(_read_stream(process.stdout, logo.log_stdout)),
         asyncio.ensure_future(_read_stream(process.stderr, logo.log_stderr))
     ]
-    await asyncio.wait(tasks)
-    ret = await process.wait()
+
+    try:
+        await asyncio.gather(*[asyncio.shield(t) for t in tasks])
+        ret = await asyncio.shield(process.wait())
+    except asyncio.CancelledError:
+        process.terminate()
+        logging.debug('Command "%s" cancelled', cmdstr)
+        await process.wait()
+        raise
 
     if ret and fail:
         msg = f'Command "{cwd}$ {cmdstr}" failed'

kas/kas.py
@@ -23,11 +23,11 @@
     This module is the main entry point for kas, setup tool for bitbake based
     projects. In case of user errors (e.g. invalid configuration, repo fetch
     failure) kas exits with error code 2, while exiting with 1 for internal
-    errors. For details on error handling, see :mod:`kas.kasusererror`.
+    errors. When cancelled by SIGINT, kas exits with 130. For details on error
+    handling, see :mod:`kas.kasusererror`.
 """
 
 import argparse
-import atexit
 import asyncio
 import traceback
 import logging
@@ -85,37 +85,37 @@ def cleanup_logger():
             logging.root.removeHandler(handler)
 
 
+def get_pending_tasks(loop):
+    try:
+        return asyncio.all_tasks(loop)
+    except AttributeError:
+        # for Python < 3.7
+        return asyncio.Task.all_tasks(loop)
+
+
 def interruption():
     """
-        Ignore SIGINT/SIGTERM in kas, let them be handled by our sub-processes
+        Gracefully cancel all tasks in the event loop
     """
-    pass
+    loop = asyncio.get_event_loop()
+    pending = get_pending_tasks(loop)
+    if pending:
+        logging.debug(f'waiting for {len(pending)} tasks to terminate')
+    [t.cancel() for t in pending]
 
 
-def _atexit_handler():
+def shutdown_loop(loop):
     """
         Waits for completion of the event loop
     """
+    pending = get_pending_tasks(loop)
+    # Ignore exceptions in final shutdown (Python3.6 workaround).
+    # These are related to the cancellation of tasks.
     try:
-        loop = asyncio.get_running_loop()
-        pending = asyncio.all_tasks(loop)
-    except RuntimeError:
-        # no running loop anymore, nothing to do
-        return
-    except AttributeError:
-        # for Python < 3.7
-        loop = asyncio.get_event_loop()
-        pending = asyncio.Task.all_tasks(loop)
-    if not loop.is_closed():
-        # this code path is observed on older python versions (e.g. 3.6).
-        # In case the loop is not yet closed, tasks still might throw
-        # exceptions, but we are not interested in these as they are
-        # likely due to the cancellation. By that, we simply drop them.
-        try:
-            loop.run_until_complete(asyncio.gather(*pending))
-        except KasUserError:
-            pass
-        loop.close()
+        loop.run_until_complete(asyncio.gather(*pending))
+    except KasUserError:
+        pass
+    loop.close()
 
 
 def kas_get_argparser():
@@ -197,7 +197,6 @@ def kas(argv):
     # don't overwrite pytest's signal handler
     if "PYTEST_CURRENT_TEST" not in os.environ:
         loop.add_signal_handler(signal.SIGINT, interruption)
-    atexit.register(_atexit_handler)
 
     try:
         plugin_class = plugins.get(args.cmd)
@@ -212,12 +211,15 @@ def kas(argv):
     except KasUserError as err:
         logging.error('%s', err)
         raise
+    except asyncio.CancelledError:
+        logging.error('kas execution cancelled')
+        raise
     except Exception as err:
         logging.error('%s', err)
         raise
     finally:
+        shutdown_loop(loop)
         cleanup_logger()
-        loop.close()
 
 
 def main():
@@ -231,6 +233,8 @@ def main():
         sys.exit(err.ret_code if err.forward else 2)
     except KasUserError:
         sys.exit(2)
+    except asyncio.CancelledError:
+        sys.exit(130)
     except Exception:
         traceback.print_exc()
         sys.exit(1)

commit eef30aa52ccbf127c670d4a8ea423b74050e04a5

fix: cancel pending tasks on exception
In case one task raises an exception that is not ignore-handled, we must
cancel all other tasks as well. Otherwise it takes quite long until the
overall execution finishes.

Fixes: f4beea37f ("graceful cancellation of external commands")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

kas/libkas.py
@@ -178,7 +178,11 @@ async def run_cmd_async(cmd, cwd, env=None, fail=True, liveupdate=False,
         await asyncio.gather(*[asyncio.shield(t) for t in tasks])
         ret = await asyncio.shield(process.wait())
     except asyncio.CancelledError:
-        process.terminate()
+        try:
+            process.terminate()
+        except ProcessLookupError:
+            # Process already exited between the cancel and us reaching here.
+            pass
         logging.debug('Command "%s" cancelled', cmdstr)
         await process.wait()
         raise
@@ -244,7 +248,11 @@ def repos_fetch(repos):
     try:
         loop.run_until_complete(asyncio.gather(*tasks))
     except CommandExecError as e:
+        [t.cancel() for t in tasks]
         raise TaskExecError('fetch repos', e.ret_code)
+    except KasUserError:
+        [t.cancel() for t in tasks]
+        raise
 
 
 def repos_apply_patches(repos):
@@ -264,7 +272,11 @@ def repos_apply_patches(repos):
     try:
         loop.run_until_complete(asyncio.gather(*tasks))
     except CommandExecError as e:
+        [t.cancel() for t in tasks]
         raise TaskExecError('apply patches', e.ret_code)
+    except KasUserError:
+        [t.cancel() for t in tasks]
+        raise
 
 
 def get_buildtools_dir():